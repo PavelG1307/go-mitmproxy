2e6f53d6bb3c6e55fbac6a5d75123c7c672c3060
PavelG1307 <julu13@yandex.ru>
2025-02-26 20:54:38 +0300

    change mod name

--- proxy/attacker.go
+++ proxy/attacker.go
@@ -9,8 +9,8 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/lqqyt2423/go-mitmproxy/cert"
-	"github.com/lqqyt2423/go-mitmproxy/internal/helper"
+	"github.com/PavelG1307/go-mitmproxy/cert"
+	"github.com/PavelG1307/go-mitmproxy/internal/helper"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/http2"
 )
